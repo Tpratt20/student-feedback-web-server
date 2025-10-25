0a14ce946555d35c72f2f040a764e096a889f55e
Tyler <Tylerpratt45@gmail.com>
2025-10-25 15:00:42 -0500

    Updates

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // use node server.js to run manually, CTRL+C to stop server, npx nodemon to allow server to update automatically
 
-
+//Imports express and path
 const express = require('express')
 const path = require('path')
+// Imports body-parser, which allows the server to read post requests by parsing the body for data and converting it to JSON
 const bodyParser = require('body-parser')
 
+// imports index.js
 const indexRouter = require('./routes/index.js')
 
 const app = express() // creates the web application server
@@ -12,6 +14,7 @@ const app = express() // creates the web application server
 // enable parsing of POST request body
 app.use(bodyParser.urlencoded({extended: false}))
 
+// Shows path and express where to find static files like stylesheets
 const staticFileLocation = path.join(__dirname, 'public')
 app.use(express.static(staticFileLocation))
 
